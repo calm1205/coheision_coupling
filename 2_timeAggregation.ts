e3258cfc0f6e4c946739cdcbb6090ae18c2e4faf
calm1205 <naginani072@gmail.com>
2022-04-29 17:24:31 +0900

    time aggregation

---
+++ 2_timeAggregation.ts
@@ -0,0 +1,12 @@
+function timeAggregation(
+  config: () => void,
+  db: () => void,
+  logger: () => void
+) {
+  // 初期化処理のように特定の時間に実行される複数の処理がまとまっている
+  // 以下の処理は機能やデータにおいて関係がない
+  // また実行順序に意味はない
+  config();
+  db();
+  logger();
+}
